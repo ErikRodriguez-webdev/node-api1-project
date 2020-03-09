334370e15ebc2e3c06bcbc41a0d26ef74d340fb8
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-09 13:52:13 -0700

    Add nodemon and shortid to start endpoints
    *Working to complete post and get endpoints
    *Resources needed and succefully setup files for server

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req, res) => {
+  res.status(200);
+});
+
 const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`API RUNNING ON localhost:${PORT}`);
